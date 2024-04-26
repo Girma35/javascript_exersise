f07908c20f7700569f87bac5333eb0a48268846b
Girma35 <girmawakeyo@gmail.com>
2024-04-26 08:53:15 +0600

    Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.

---
+++ area of triangle/index.js
@@ -0,0 +1,6 @@
+const side1=5;
+const side2=6;
+const side3=7;
+let s =(side1+side2+side3)/2;
+let Area =Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
+document.writeln("the area of the triangle is:"+Area);
